ca505f8ce484edd9262b2c62b595cfa608e3f131
Nathaniel Mason <n11412402@qut.edu.au>
2024-11-28 11:36:30 +1000

    initial page rework

--- gym-run/app/page.tsx
+++ gym-run/app/page.tsx
@@ -1,15 +1,8 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
 export default async function Index() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <h1>Gym + Run Scheduling Made Easy, Powered by AI</h1>
       </main>
     </>
   );
